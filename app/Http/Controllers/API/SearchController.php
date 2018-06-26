06020674898ae1466397b82081a0a8311cce2f92
aftab <aftab.official02@gmail.com>
2018-06-26 18:30:02 +0530

    first commit

---
+++ app/Http/Controllers/API/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class SearchController extends Controller
+{
+    public $successStatus = 200;
+
+    public function searchUser(Request $request)
+    {
+        $user = auth::user();
+        $text = $request->get('text');
+        $searchUser = DB::table('users')->where('name','like','%'.$text.'%')->get();
+
+        return response()->json(['success' => $searchUser], $this->successStatus);
+    }
+}
